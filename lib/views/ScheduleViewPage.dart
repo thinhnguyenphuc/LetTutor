0de089bbdb582ef1599891de5e78fff141247ef7
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2022-04-15 02:13:00 +0700

    Fix scroll schedule

--- lib/views/ScheduleViewPage.dart
+++ lib/views/ScheduleViewPage.dart
@@ -24,23 +24,26 @@ class _ScheduleScreenPageState extends State<ScheduleScreen> {
   @override
   Widget build(BuildContext context) {
     final scheduleListOnProvider = Provider.of<ScheduleViewModel>(context);
-    final totalLearnedTime = scheduleListOnProvider.totalLearnedTime.isNotEmpty? Text("Total lesson time is "+scheduleListOnProvider.totalLearnedTime,
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30,
-        )) : const Center(
-      child: SizedBox(
-        height: 50,
-        width: 50,
-        child: CircularProgressIndicator(),
-      ),
-    );
+    final totalLearnedTime = scheduleListOnProvider.totalLearnedTime.isNotEmpty
+        ? Text(
+            "Total lesson time is " + scheduleListOnProvider.totalLearnedTime,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ))
+        : const Center(
+            child: SizedBox(
+              height: 50,
+              width: 50,
+              child: CircularProgressIndicator(),
+            ),
+          );
 
     final scheduleListView = scheduleListOnProvider.nextSchedule.isNotEmpty
         ? ListView.builder(
-            physics: const NeverScrollableScrollPhysics(),
+            physics: const AlwaysScrollableScrollPhysics(),
             shrinkWrap: true,
             itemCount: scheduleListOnProvider.nextSchedule.length,
             itemBuilder: (context, position) {
@@ -62,118 +65,115 @@ class _ScheduleScreenPageState extends State<ScheduleScreen> {
             ),
           );
     return Scaffold(
-        backgroundColor: Colors.white12,
-        resizeToAvoidBottomInset: false,
-        body: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).requestFocus(FocusNode());
-          },
-          child: SingleChildScrollView(
-            physics: const ScrollPhysics(),
-            child: Column(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 4,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                      borderRadius: const BorderRadius.all(Radius.circular(
-                              5.0) //                 <--- border radius here
+      backgroundColor: Colors.white12,
+      resizeToAvoidBottomInset: false,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 4,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(
+                      5.0) //                 <--- border radius here
+                  ),
+                  color: Colors.blueGrey.shade500),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(children: [
+                          Row(
+                            children: [
+                              Container(
+                                width:
+                                MediaQuery.of(context).size.width / 1.1,
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  children: <Widget>[totalLearnedTime],
+                                ),
+                              ),
+                            ],
                           ),
-                      color: Colors.blueGrey.shade500),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Column(children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    width:
-                                        MediaQuery.of(context).size.width / 1.1,
-                                    padding: const EdgeInsets.all(16.0),
-                                    child: Column(
-                                      children: <Widget>[
-                                        totalLearnedTime
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                          Row(
+                            children: [
+                              Container(
+                                width:
+                                MediaQuery.of(context).size.width / 1.1,
+                                padding: const EdgeInsets.fromLTRB(
+                                    16.0, 0, 16, 16),
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(Strings.noUpcoming,
+                                        textAlign: TextAlign.center,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        )),
+                                  ],
+                                ),
                               ),
-                              Row(
-                                children: [
-                                  Container(
-                                    width:
-                                        MediaQuery.of(context).size.width / 1.1,
-                                    padding: const EdgeInsets.fromLTRB(
-                                        16.0, 0, 16, 16),
-                                    child: Column(
-                                      children: <Widget>[
-                                        Text(Strings.noUpcoming,
-                                            textAlign: TextAlign.center,
-                                            style: const TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 20,
-                                            )),
-                                      ],
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.date_range,
+                                      color: Colors.blueGrey,
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  ElevatedButton(
-                                    onPressed: () {},
-                                    child: Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.date_range,
-                                          color: Colors.blueGrey,
-                                        ),
-                                        Text(Strings.bookLesson,
-                                            style: const TextStyle(
-                                                color: Colors.blueGrey))
-                                      ],
-                                    ),
-                                    style: ButtonStyle(
-                                        backgroundColor: MaterialStateProperty
-                                            .resolveWith<Color>(
-                                                (Set<MaterialState> states) {
-                                          if (states.contains(
-                                              MaterialState.pressed)) {
+                                    Text(Strings.bookLesson,
+                                        style: const TextStyle(
+                                            color: Colors.blueGrey))
+                                  ],
+                                ),
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty
+                                        .resolveWith<Color>(
+                                            (Set<MaterialState> states) {
+                                          if (states
+                                              .contains(MaterialState.pressed)) {
                                             return Colors.white54;
                                           }
                                           return Colors
                                               .white; // Use the component's default.
                                         }),
-                                        shape: MaterialStateProperty.all<
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(28.0),
-                                                side: const BorderSide(
-                                                    color: Colors.blue)))),
-                                  )
-                                ],
-                              ),
-                            ]),
-                          ])
-                    ],
-                  ),
-                ),
-                scheduleListView
-              ],
+                                    shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                            BorderRadius.circular(28.0),
+                                            side: const BorderSide(
+                                                color: Colors.blue)))),
+                              )
+                            ],
+                          ),
+                        ]),
+                      ])
+                ],
+              ),
             ),
-          ),
+            Expanded(
+                child: scheduleListView
+            ),
+          ],
         ),
+      ),
     );
   }
 }
